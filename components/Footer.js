7938e5fa67fa6754277eb1d9bd2936923bc062c5
Quentin Ng <quentin.ng.dev@gmail.com>
2021-10-12 22:55:00 -0700

    remove shadow logos

--- components/Footer.js
+++ components/Footer.js
@@ -9,20 +9,20 @@ const Footer = () => {
       <div className="grid grid-cols-3 items-center w-full h-full">
         <div className="flex h-full justify-center align-middle">
           <SocialIcon
-            className="hover:scale-110 transform transition-all duration-125 shadow-xl"
+            className="hover:scale-110 transform transition-all duration-125"
             bgColor="gray"
             url="https://www.github.com/kwantaing"
           ></SocialIcon>
         </div>
         <div className="flex h-full justify-center align-middle">
           <SocialIcon
-            className="hover:scale-110 transform transition-all duration-125 shadow-xl"
+            className="hover:scale-110 transform transition-all duration-125"
             url="https://www.linkedin.com/in/kwan-tai-quentin-ng-29349bbb"
           />
         </div>
         <div className="flex h-full justify-center align-middle">
           <SocialIcon
-            className="hover:scale-110 transform transition-all duration-125 shadow-xl"
+            className="hover:scale-110 transform transition-all duration-125"
             url="mailto:quentin.ng.dev@gmail.com"
           />
         </div>
