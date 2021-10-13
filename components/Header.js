e76a2e67c6478fa76f6f1b74ea30a2a9e66d7466
Quentin Ng <quentin.ng.dev@gmail.com>
2021-10-13 00:37:46 -0700

    reorder header links

--- components/Header.js
+++ components/Header.js
@@ -44,21 +44,21 @@ const Header = () => {
         />
       </div>
       <span />
-      <div className="hidden sm:inline-flex text-md font-semibold transform transition duration-500 ease-in-out col-span-2  flex space-x-2 justify-end items-center">
+      <div className="hidden sm:inline-flex text-md font-semibold transform transition duration-500 ease-in-out col-span-2  flex space-x-3 justify-end items-center">
         <a className="cursor-pointer" onClick={() => scrollToEle("about")}>
           About
-        </a>
-        <a
-          className="cursor-pointer"
-          onClick={() => scrollToEle("experiences")}
-        >
-          Experiences
         </a>
         <a className="cursor-pointer" onClick={() => scrollToEle("skills")}>
           Tech
         </a>
         <a className="cursor-pointer" onClick={() => scrollToEle("projects")}>
           Projects
+        </a>
+        <a
+          className="cursor-pointer"
+          onClick={() => scrollToEle("experiences")}
+        >
+          Experiences
         </a>
       </div>
     </header>
