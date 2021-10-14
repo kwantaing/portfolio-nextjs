680027d8fa3c72b9e8ffb54647e1dfba5d9719ce
Quentin Ng <quentin.ng.dev@gmail.com>
2021-10-13 17:13:52 -0700

    typewriter effect

--- components/About.js
+++ components/About.js
@@ -2,56 +2,46 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 const About = () => {
+  const text1 = "Hi, I'm Quentin.";
+  const text2 = "I'm a Software Engineer.";
+  const speed = 75;
+  let i = 0;
+  let j = 0;
   const resumeUrl =
     "https://portfolio-data-qdev.s3.us-east-2.amazonaws.com/quentin-ng-swe-resume.pdf";
   const gotoResume = () => {
     window.open(resumeUrl, "_blank");
   };
-
-  const getRandomInt = () => {
-    return Math.floor(Math.random() * 6);
+  const typeWriter = () => {
+    if (i < text1.length) {
+      document.getElementById("text1").innerHTML += text1.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    } else {
+      if (j < text2.length) {
+        document.getElementById("text2").innerHTML += text2.charAt(j);
+        j++;
+        setTimeout(typeWriter, speed);
+      }
+    }
   };
-  const gradient = {
-    direction: [
-      "bg-gradient-to-br",
-      "bg-gradient-to-b",
-      "bg-gradient-to-bl",
-      "bg-gradient-to-r",
-      "bg-gradient-to-tr",
-      "bg-gradient-to-bl",
-    ],
-    colors: ["blue", "green", "red", "yellow", "pink", "purple"],
-    weight: ["100", "200", "300", "400", "600", "800"],
-  };
-  const randomGradient = () => {
-    let direction = gradient.direction[getRandomInt()];
-    let from = `from-${gradient.colors[getRandomInt()]}-${
-      gradient.weight[getRandomInt()]
-    }`;
-    let via = `via-${gradient.colors[getRandomInt()]}-${
-      gradient.weight[getRandomInt()]
-    }`;
-    let to = `to-${gradient.colors[getRandomInt()]}-${
-      gradient.weight[getRandomInt()]
-    }`;
-    return `${direction} ${from} ${via} ${to}`;
-  };
+  useEffect(() => {
+    typeWriter();
+  }, []);
   return (
     <div
-    id="about"
+      id="about"
       className={`bg-gradient-to-br from-purple-200 via-purple-400 to-blue-400 relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]`}
     >
-      {/* <Image
-        src="https://links.papareact.com/0fm"
-        layout="fill"
-        objectFit="cover"
-    /> */}
       <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg font-semibold text-gray-200">
-          Hi, I'm Quentin.
-        </p>
-        <p className="text-sm sm:text-lg font-semibold text-gray-200">
-          I'm a Software Engineer.
+        <p
+          id="text1"
+          className="text-sm sm:text-lg font-semibold text-gray-200"
+        ></p>
+        <p
+          id="text2"
+          className="text-sm sm:text-lg font-semibold text-gray-200"
+        >
         </p>
         <button
           onClick={() => gotoResume()}
